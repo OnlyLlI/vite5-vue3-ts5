fe71fe54cd4710f14d47a95ef9a219829f2a708a
yipeng <2602280163@qq.com>
2024-06-16 16:25:26 +0800

    feat(项目): 工程搭建

---
+++ doce/index.md
@@ -0,0 +1,23 @@
+# 项目文档
+
+### 
+
+### 项目简介
+
+这是一个使用Vite(5.2.0)+Vue.js(3.4.21)+Typescript(5.2.2)搭建的项目
+
+
+
+### 安装和运行
+
+确保你按照了Node.js(14.18+,  16+)，和Npm，改项目基于Node(20.14.0)、Npm(10.7.0)开发
+
+
+
+
+
+```bash
+npm install
+npm run dev 或者 vite dev
+```
+
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="cur-div">
+    <span class="cur-span">1111111111111</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.cur-div {
+  .cur-span{
+    color: $theme-primary;
+    font-size: 20px;
+  }
+}
+</style>
